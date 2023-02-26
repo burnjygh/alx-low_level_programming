3134338d284c065cb41897ff942e8e8fe4480ca4
Benjamin Clinton <benjaminclinton13@gmail.com>
2023-02-26 08:16:26 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - Determines number that are positive, negative or zero.
  *
  * Return: Always 0 (Success)
  */
