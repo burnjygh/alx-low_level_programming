cb4dea16203f7aa5a661f5ad9968c1efb92c0ea2
Benjamin Clinton <benjaminclinton13@gmail.com>
2023-02-26 08:30:55 -0800

    Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * main - Prints the alphabet without q and e.
+ * main - Prints the alphabet but omit q and e.
  *
  * Return: Always 0 (Success)
  */
